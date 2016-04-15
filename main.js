127ca53675ee01402dbc3277dad9c2ef9b74d141
Pratik <lazymonk@Pratiks-MacBook-Air.local>
2016-04-15 00:18:33 -0700

    not even mvp

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 $(document).ready(init);
-
 function init() {
 	$('.towers').click(function(){
-		if($(this).find(".blocks").length){
-			$('.blocks:last-child').toggleClass("selected");
-		}
-		
-	});
+		var currentBlock = $(this).find('.blocks:last-child');
+		currentBlock.toggleClass("selected");
+		secondClick(currentBlock);
+		// currentBlock = $(this).find('.blocks:last-child');
+		// currentBlock.toggleClass("selected");
+	});  
 }
+function secondClick(thisBlock) {
+	$('.towers').click(function(){
+		var currentBlock = $(this).find('.blocks:last-child');
+		if(!currentBlock.length && thisBlock.hasClass("selected")){
+			$(this).append(thisBlock);
+			thisBlock.toggleClass("selected");
+			$('.towers').unbind('click');
+			init();	
+		}
+		else if(currentBlock.length && thisBlock.hasClass("selected")){
+			if(currentBlock.width() > thisBlock.width()){
+				$(this).append(thisBlock);
+				thisBlock.toggleClass("selected");
+				$('.towers').unbind('click');
+				init();
+			}
+			else {
+				thisBlock.toggleClass("selected");
+				$('.towers').unbind('click');
+				init();
+			}
+			
+		}
+	});
+}	
+
+
